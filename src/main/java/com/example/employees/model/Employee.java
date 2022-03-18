781b24deb19ae6915aa9c6104fb05e6fa43a7eb5
Ibrahim-Asiri <i-m-6@hotmail.com>
2022-03-18 11:49:00 +0300

    slove merge

--- src/main/java/com/example/employees/model/Employee.java
+++ src/main/java/com/example/employees/model/Employee.java
@@ -16,13 +16,7 @@ public class Employee {
     private String name;
     @NotNull(message = "Age is empty!")
     private Integer age;
-<<<<<<< HEAD
     @NotNull(message = "Slary is empty!")
-||||||| b83bf47
-    @NotEmpty(message = "Slary is empty!")
-=======
-    @NotNull(message = "Salary is empty!")
->>>>>>> ebd1f8aab8eab6e7d64e6a7fe4f4e9b5d0ecda9e
     private Double salary;
 
 }
